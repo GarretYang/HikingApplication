df21c08378d142f4f1d502b10e804055095871ea
Garret Yang <GarretYang>
2019-10-13 20:05:12 -0500

    changed mongoDatabase.py

--- mongoDatabase.py
+++ mongoDatabase.py
@@ -1,5 +1,5 @@
 import pymongo
 
 # Mongodb client
-client = pymongo.MongoClient("mongodb+srv://GarretYang:1lNZD6YooaYrYRSd@cluster0-tohqa.mongodb.net/test?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb+srv://username:password@cluster0-tohqa.mongodb.net/test?retryWrites=true&w=majority")
 db = client['hiking']
