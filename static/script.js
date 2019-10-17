c6a3a3837277fc9b58f7f61d2d1cfd2d91be3150
Yang Hu <yang.hu@utexas.edu>
2019-10-16 19:32:11 -0500

    update Facebook login and UI

--- static/script.js
+++ static/script.js
@@ -32,6 +32,10 @@ window.addEventListener('load', function () {
       // the Firebase console.
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
       firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      //firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      //firebase.auth.PhoneAuthProvider.PROVIDER_ID
     ],
     // Terms of service url.
     tosUrl: '<your-tos-url>'
