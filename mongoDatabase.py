162f22826b19b8167bfa531ec2828fa192595cf8
CCCCChloe <tpanchloe@gmail.com>
2019-10-19 23:45:55 -0500

    update UI: add card style

--- mongoDatabase.py
+++ mongoDatabase.py
@@ -3,7 +3,7 @@ from google.auth.transport import requests
 
 
 # Mongodb client
-client = pymongo.MongoClient("mongodb+srv://username:password@cluster0-tohqa.mongodb.net/test?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb+srv://TeamTwo:team12345two@cluster0-tohqa.mongodb.net/test?retryWrites=true&w=majority")
 db = client['hiking']
 
 # Firebase authentication
