b88ccae4059d104edf5f50a1b7ba15aed804442a
CCCCChloe <tpanchloe@gmail.com>
2019-10-20 15:03:23 -0500

    search by tags

--- static/js/single_theme.js
+++ static/js/single_theme.js
@@ -3,11 +3,25 @@ function search(){
     reports = document.getElementsByClassName("report-box");
 
     for (i = 0; i < reports.length; i++) {
-        reports_name = reports[i].getElementsByClassName("report-description")[0].innerHTML;
+        reports[i].style.display = "none";
+        tags = reports[i].getElementsByClassName("tag");
+        for (j = 0; j < tags.length; j++) {
+            if (tags[j].innerHTML.toUpperCase().indexOf(input) > -1) {
+                
+                reports[i].style.display = "";
+            }
+        }
+        /*reports_name = reports[i].getElementsByClassName("report-description")[0].innerHTML;
         if (reports_name.toUpperCase().indexOf(input) > -1) {
             reports[i].style.display = "";
         } else {
             reports[i].style.display = "none";
+        }*/
+    }
+
+    if (input == '') {
+        for (i = 0; i < reports.length; i++) {
+            reports[i].style.display = "";
         }
     }
     document.getElementById('reports').scrollIntoView();
