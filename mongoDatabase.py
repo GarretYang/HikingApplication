70766e2e9731c3f00270aaeb7f55a72fcaeb6885
Yang Hu <yang.hu@utexas.edu>
2019-10-27 20:15:00 -0500

    update test database

--- mongoDatabase.py
+++ mongoDatabase.py
@@ -4,7 +4,7 @@ from google.auth.transport import requests
 
 # Mongodb client
 client = pymongo.MongoClient("mongodb+srv://TeamTwo:team12345two@cluster0-tohqa.mongodb.net/test?retryWrites=true&w=majority")
-db = client['hiking']
+db = client['hiking_test']
 
 # Firebase authentication
 firebase_request_adapter = requests.Request()
