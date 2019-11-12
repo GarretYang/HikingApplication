46ee2f1c79a26adf998db17335fe41c315e2dce8
Dylan Bray <dfbray@outlook.com>
2019-11-11 20:01:13 -0600

    Blank map appears on maps page

--- react-native/HikingApp/components/map/MapPage.js
+++ react-native/HikingApp/components/map/MapPage.js
@@ -3,13 +3,27 @@ import React from 'react';
 //import react in our code.
 import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
 //import all the basic component we have used
- 
+
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+
 export default class MapPage extends React.Component {
   //Home Screen to show in Home Option
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text style={{ marginTop: 50, fontSize: 25 }}>Map</Text>
+                <View style={styles.container}>
+                  <MapView
+                    provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+                    style={styles.map}
+                    region={{
+                      latitude: 30.288459,
+                      longitude: -97.735134,
+                      latitudeDelta: 0.015,
+                      longitudeDelta: 0.0121,
+                    }}
+                    >
+                  </MapView>
+                </View>
       </View>
     );
   }
@@ -22,4 +36,14 @@ const styles = StyleSheet.create({
     width: 300,
     marginTop: 16,
   },
+   container: {
+     ...StyleSheet.absoluteFillObject,
+     height: 600,
+     width: 400,
+     justifyContent: 'flex-end',
+     alignItems: 'center',
+   },
+   map: {
+     ...StyleSheet.absoluteFillObject,
+   },
 });
