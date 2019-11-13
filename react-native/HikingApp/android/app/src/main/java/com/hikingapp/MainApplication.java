6347bb893d0ca7cdc266cb242c5836dcfb1a27f0
CCCCChloe <tpanchloe@gmail.com>
2019-11-13 13:33:29 -0600

    show upload photo

--- react-native/HikingApp/android/app/src/main/java/com/hikingapp/MainApplication.java
+++ react-native/HikingApp/android/app/src/main/java/com/hikingapp/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import fr.snapp.imagebase64.RNImgToBase64Package;
 import com.swmansion.reanimated.ReanimatedPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.imagepicker.ImagePickerPackage;
