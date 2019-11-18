23fb36ad6fecb7801911e6c8884f0227f12dfa41
Dylan Bray <dfbray@outlook.com>
2019-11-18 16:42:03 -0600

    Working on markers

--- react-native/HikingApp/components/map/MapPage.js
+++ react-native/HikingApp/components/map/MapPage.js
@@ -8,25 +8,63 @@ import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 export default class MapPage extends React.Component {
   //Home Screen to show in Home Option
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      markers: [{
+        coordinates: {
+          latitude: 30.28,
+          longitude: -97.74
+        },
+        title: "TITLE!!",
+        description: "DESC!!"
+      }],
+      markers2: getReports()
+    }
+  }
+
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <View style={styles.container}>
-                  <MapView
-                    provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-                    style={styles.map}
-                    region={{
-                      latitude: 30.288459,
-                      longitude: -97.735134,
-                      latitudeDelta: 0.015,
-                      longitudeDelta: 0.0121,
-                    }}
-                    >
-                  </MapView>
-                </View>
+        <View style={styles.container}>
+          <MapView
+            provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+            style={styles.map}
+            region={{
+              latitude: 30.288459,
+              longitude: -97.735134,
+              latitudeDelta: 0.015,
+              longitudeDelta: 0.0121,
+            }}
+            >
+            {this.state.markers.map(marker => (
+              console.log("MARKER!!"),
+              <MapView.Marker
+                coordinate={marker.coordinates}
+                title={marker.title}
+                description={marker.description}
+              />
+            ))}
+          </MapView>
+        </View>
       </View>
     );
   }
+}
+
+function getReports() {
+  return fetch('http://aptproject-255903.appspot.com/locations')
+    .then((response) => response.json())
+    .then((responseJson) => {
+      console.log(responseJson);
+      console.log("inside responsejson");
+      return responseJson;
+    })
+    .catch((error) => {
+      console.log(error)
+      console.error(error);
+    });
 }
 const styles = StyleSheet.create({
   button: {
